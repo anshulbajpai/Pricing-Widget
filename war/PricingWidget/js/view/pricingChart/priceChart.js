65713bce942b2ca249266ea8a477daf5e006ae0b
Anshul Bajpai <bajpai.anshul@gmail.com>
2011-03-15 01:09:02 +0530

    Revert "Resolving memory issue <Anshul>"
    This reverts commit a5148c925b9035f976acccd1b2ec2b6586e9ce90.

--- war/PricingWidget/js/view/pricingChart/priceChart.js
+++ war/PricingWidget/js/view/pricingChart/priceChart.js
@@ -1,8 +1,7 @@
 var PriceChart = function (containerId) {
     this.data = this._createFreshPriceChartViewModel();
     this.containerId = containerId;
-	this.options = {series : { map: { pointDimension: 3, active: true, show: true}}, xaxis : {noTicks : true}};
-	this.plot = null;
+	this.options = {series : { map: { pointDimension: 3, active: true, show: true}}, xaxis : {noTicks : true}};	
 };
 
 PriceChart.prototype.MAX_SERIES = 112;
@@ -13,7 +12,6 @@ PriceChart.prototype.setDataPoint = function(seriesNumber, pointId, dataPoint) {
 	
 PriceChart.prototype.reset = function(){
 	this.data = this._createFreshPriceChartViewModel();
-	this.plot = null;
 };
 
 PriceChart.prototype._createFreshPriceChartViewModel = function(){
@@ -24,12 +22,5 @@ PriceChart.prototype.drawChart = function() {
 	var data = this.data.getData();
 	var priceBound = data.priceBound;
 	this.options.yaxis = {min : priceBound.minPrice, max : priceBound.maxPrice};
-	if(!this.plot)
-		this.plot = $.plot($(this.containerId),data.dataPoints , this.options);
-	else{
-		this.plot.getOptions().yaxis = this.options.yaxis;
-		this.plot.setData(data.dataPoints);
-		this.plot.setupGrid();
-		this.plot.draw();		
-	}
+	$.plot($(this.containerId),data.dataPoints , this.options);
 };
