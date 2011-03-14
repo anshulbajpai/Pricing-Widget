a5148c925b9035f976acccd1b2ec2b6586e9ce90
Anshul Bajpai <bajpai.anshul@gmail.com>
2011-03-15 00:59:23 +0530

    Resolving memory issue <Anshul>

--- war/PricingWidget/js/view/pricingChart/priceChart.js
+++ war/PricingWidget/js/view/pricingChart/priceChart.js
@@ -1,7 +1,8 @@
 var PriceChart = function (containerId) {
     this.data = this._createFreshPriceChartViewModel();
     this.containerId = containerId;
-	this.options = {series : { map: { pointDimension: 3, active: true, show: true}}, xaxis : {noTicks : true}};	
+	this.options = {series : { map: { pointDimension: 3, active: true, show: true}}, xaxis : {noTicks : true}};
+	this.plot = null;
 };
 
 PriceChart.prototype.MAX_SERIES = 112;
@@ -12,6 +13,7 @@ PriceChart.prototype.setDataPoint = function(seriesNumber, pointId, dataPoint) {
 	
 PriceChart.prototype.reset = function(){
 	this.data = this._createFreshPriceChartViewModel();
+	this.plot = null;
 };
 
 PriceChart.prototype._createFreshPriceChartViewModel = function(){
@@ -22,5 +24,12 @@ PriceChart.prototype.drawChart = function() {
 	var data = this.data.getData();
 	var priceBound = data.priceBound;
 	this.options.yaxis = {min : priceBound.minPrice, max : priceBound.maxPrice};
-	$.plot($(this.containerId),data.dataPoints , this.options);
+	if(!this.plot)
+		this.plot = $.plot($(this.containerId),data.dataPoints , this.options);
+	else{
+		this.plot.getOptions().yaxis = this.options.yaxis;
+		this.plot.setData(data.dataPoints);
+		this.plot.setupGrid();
+		this.plot.draw();		
+	}
 };
