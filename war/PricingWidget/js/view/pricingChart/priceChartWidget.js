c5d2b643668441cabf92713860fac4bb50e6e3a7
Anshul Bajpai <bajpai.anshul@gmail.com>
2011-03-15 11:39:26 +0530

    Increased color intensity for lower values <Anshul>

--- war/PricingWidget/js/view/pricingChart/priceChartWidget.js
+++ war/PricingWidget/js/view/pricingChart/priceChartWidget.js
@@ -7,14 +7,14 @@ PriceChartWidget.prototype.update = function(pricingModel) {
 	var askData = pricingModel.askData;
 	var bidData = pricingModel.bidData;	
 	
-	var bidColorFactor = this._getColorFactorFor(bidData);
-	var askColorFactor = this._getColorFactorFor(askData);
+	var bidMaxValue = this._getMaxValueFor(bidData);
+	var askMaxValue = this._getMaxValueFor(askData);
 	
 	for(var i =0; i < bidData.length; i++){
-		this._updateGraphData(bidData[i], bidColorFactor, i, false);
+		this._updateGraphData(bidData[i], bidMaxValue, i, false);
 	}
 	for(var j = 0; j < askData.length; j++){
-		this._updateGraphData(askData[j], askColorFactor, i+j, true);
+		this._updateGraphData(askData[j], askMaxValue, i+j, true);
 	}
 	this.priceChart.drawChart();
 	this.count++;
@@ -25,22 +25,24 @@ PriceChartWidget.prototype.reset = function(){
 	this.priceChart.reset();
 };
 
-PriceChartWidget.prototype._getColorFactorFor = function(dataArray) {
-	var max = 0;
-	var min = Number.MAX_VALUE;
+PriceChartWidget.prototype._getMaxValueFor = function(dataArray) {
+	var max = 0;	
 	for (var i = 0; i < dataArray.length; i++) {
 		var priceData = dataArray[i];
 		if (priceData.quantity > max) {
 			max = priceData.quantity;
 		}
-		if (priceData.quantity < min) {
-			min = priceData.quantity;
-		}
-	}
-	return max - min;
+	}	
+	return max;
 };
 
-PriceChartWidget.prototype._updateGraphData = function(pricePoint, colorFactor, pointId, isAskPoint) {
+PriceChartWidget.prototype._updateGraphData = function(pricePoint, maxQuantity, pointId, isAskPoint) {
 	var maxSeries = this.priceChart.MAX_SERIES;
-	this.priceChart.setDataPoint(this.count%maxSeries, pointId, [this.count%maxSeries, pricePoint.price, pricePoint.quantity/colorFactor , isAskPoint]);
+	this.priceChart.setDataPoint(this.count%maxSeries, pointId, [this.count%maxSeries, pricePoint.price, this._getColorFactorFor(pricePoint.quantity, maxQuantity), isAskPoint]);
 };
+
+PriceChartWidget.prototype._getColorFactorFor = function(quantity, maxQuantity){
+	return ((quantity/maxQuantity)*0.8) + 0.2;
+};
+
+
